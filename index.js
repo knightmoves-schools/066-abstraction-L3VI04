a746331fdaadcb231aa56acc87bc045067601fba
L3VI04 <ljenkins@anamosa.k12.ia.us>
2025-11-20 13:36:28 -0600

    Refactor TaxCalculator to use private fields and methods

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
-class TaxCalculator{
-    
-    constructor(){
-        this.rate = 15;
-        this.state = 'TX';
-        this.exempt = false;
+// index.js
+class TaxCalculator {
+  // private field
+  #rate = 15;
+
+  constructor() {
+    this.state = 'TX';
+    this.exempt = false;
+  }
+
+  // private methods
+  #calculateExempt(multiplier) {
+    return multiplier * this.#rate;
+  }
+
+  #calculateNonExempt(multiplier) {
+    return multiplier * this.#rate;
+  }
+
+  // public method
+  calculate() {
+    if (this.exempt) {
+      return this.#calculateExempt(1.37);
+    } else {
+      return this.#calculateNonExempt(5.72);
     }
-    
-    calculate(){
-        if(this.exempt){
-            return calculateExempt(1.37);
-        }else{
-            return calculateNonExempt(5.72);
-        }
-    }
+  }
 }
+
+module.exports = TaxCalculator;
